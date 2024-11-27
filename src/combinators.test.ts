c62345f2d8ab698ab74162c9140e010e6487c3fd
wagao <wagao1029@gmail.com>
2024-11-28 01:11:05 +0900

    feat: Add or combinator func

--- src/combinators.test.ts
+++ src/combinators.test.ts
@@ -1,5 +1,5 @@
 import { char } from './char';
-import { not } from './combinators';
+import { not, or } from './combinators';
 
 import type { ParserOutput } from './types';
 
@@ -41,6 +41,68 @@ describe('not(char("a"))', () => {
       result: 'success',
       data: null,
       rest: [...'hoge']
+    });
+  });
+});
+
+describe('or()', () => {
+  describe('or([])', () => {
+    const parser = or([]);
+
+    test('Empty input', () => {
+      const input = [] as const;
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<unknown>>({
+        result: 'fail'
+      });
+    });
+
+    test('Input "a"', () => {
+      const input = [...'a'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<unknown>>({
+        result: 'fail'
+      });
+    });
+  });
+
+  describe('or([char("a"), char("b")])', () => {
+    const parser = or([char('a'), char('b')]);
+
+    test('Empty input', () => {
+      const input = [] as const;
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a' | 'b'>>({
+        result: 'fail'
+      });
+    });
+
+    test('Input "a"', () => {
+      const input = [...'a'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a' | 'b'>>({
+        result: 'success',
+        data: 'a',
+        rest: []
+      });
+    });
+
+    test('Input "b"', () => {
+      const input = [...'b'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a' | 'b'>>({
+        result: 'success',
+        data: 'b',
+        rest: []
+      });
+    });
+
+    test('Input "A"', () => {
+      const input = [...'A'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a' | 'b'>>({
+        result: 'fail'
+      });
     });
   });
 });
