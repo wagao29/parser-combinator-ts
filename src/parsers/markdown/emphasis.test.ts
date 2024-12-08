5f92cc5bc6ad2c4c338e4355446f11989443ee3d
wagao <wagao1029@gmail.com>
2024-12-08 19:41:21 +0900

    feat: Add markdown emphasis parser

---
+++ src/parsers/markdown/emphasis.test.ts
@@ -0,0 +1,50 @@
+import type { Emphasis } from 'mdast';
+
+import type { ParserOutput } from '../../types';
+import { emphasis } from './emphasis';
+
+describe('emphasis', () => {
+  const parser = emphasis;
+
+  test('Empty input', () => {
+    const input = [] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<Emphasis>>({
+      result: 'fail'
+    });
+  });
+
+  test('Input "*emphasis text"', () => {
+    const input = [...'*emphasis text'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<Emphasis>>({
+      result: 'fail'
+    });
+  });
+
+  test('Input "emphasis text*"', () => {
+    const input = [...'emphasis text*'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<Emphasis>>({
+      result: 'fail'
+    });
+  });
+
+  test('Input "*emphasis text*"', () => {
+    const input = [...'*emphasis text*'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<Emphasis>>({
+      result: 'success',
+      data: {
+        type: 'emphasis',
+        children: [
+          {
+            type: 'text',
+            value: 'emphasis text'
+          }
+        ]
+      },
+      rest: []
+    });
+  });
+});
---
+++ src/parsers/markdown/emphasis.ts
@@ -0,0 +1,25 @@
+import type { Emphasis } from 'mdast';
+import { char } from '../../char';
+import { cat, rep } from '../../combinators';
+import { anyChar } from '../../primitives';
+import type { ParserInput, ParserOutput } from '../../types';
+import { diff, map } from '../../util';
+
+// TODO: Supports types other than text for children
+export function emphasis(input: ParserInput): ParserOutput<Emphasis> {
+  return map(
+    cat([char('*'), rep(diff(anyChar, char('*'))), char('*')]),
+    ([, v]) => {
+      const emphasis: Emphasis = {
+        type: 'emphasis',
+        children: [
+          {
+            type: 'text',
+            value: v.join('')
+          }
+        ]
+      };
+      return emphasis;
+    }
+  )(input);
+}
