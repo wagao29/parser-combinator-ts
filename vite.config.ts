6397bebfe4c827c590dd677ed2a510598a795300
wagao <wagao1029@gmail.com>
2024-12-13 23:41:50 +0900

    chore: Migrate from Jest to Vitest

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+
+// biome-ignore lint/style/noDefaultExport: <explanation>
+export default defineConfig({
+  test: {
+    globals: true
+  }
+});
